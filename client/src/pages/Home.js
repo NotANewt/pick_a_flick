6df3e2e737efcb9b609015c4369fb86117f502ec
NotANewt <meegafauna@gmail.com>
2022-05-10 13:19:05 -0500

    added LoginForm and SignupForm components and linked them to the Home page

---
+++ client/src/pages/Home.js
@@ -0,0 +1,12 @@
+import LoginForm from "../components/LoginForm";
+import SignupForm from "../components/SignupForm";
+
+function Home(props) {
+  return (
+    <>
+      <LoginForm></LoginForm>
+      <SignupForm></SignupForm>
+    </>
+  );
+}
+export default Home;
