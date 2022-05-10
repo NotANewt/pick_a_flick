2f6313412f63bf3c6ea155d696f8fbb16546a46d
NotANewt <meegafauna@gmail.com>
2022-05-10 15:56:01 -0500

    created LoginForm component and imported component to LoginSignup page

--- client/src/pages/LoginSignup.js
+++ client/src/pages/LoginSignup.js
@@ -1,10 +1,12 @@
 import SignupForm from "../components/SignupForm";
+import LoginForm from "../components/LoginForm";
 
 function LoginSignup(props) {
   return (
     <>
       <h1>This is the Login/Signup page</h1>
       <SignupForm></SignupForm>
+      <LoginForm></LoginForm>
     </>
   );
 }
