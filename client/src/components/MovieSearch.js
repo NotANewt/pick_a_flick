8db660a7b20ff5b38161141b9fe76e8941da98c4
NotANewt <meegafauna@gmail.com>
2022-05-11 14:22:54 -0500

    added MovieSearch component and improted it into the Profile page

---
+++ client/src/components/MovieSearch.js
@@ -0,0 +1,9 @@
+function MovieSearch(props) {
+  return (
+    <>
+      <h2>MovieSearch goes here!</h2>
+    </>
+  );
+}
+
+export default MovieSearch;
--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,10 +1,12 @@
 import Dealbreakers from "../components/Dealbreakers";
+import MovieSearch from "../components/MovieSearch";
 
 function Profile(props) {
   return (
     <>
       <h1>This is the Profile Page</h1>
       <Dealbreakers></Dealbreakers>
+      <MovieSearch></MovieSearch>
     </>
   );
 }
