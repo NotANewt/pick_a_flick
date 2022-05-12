1fe4e269b61598549e7d9cbca6efded044ddbd46
NotANewt <meegafauna@gmail.com>
2022-05-12 17:27:21 -0500

    working on movie details page

--- client/src/pages/MovieDetails.js
+++ client/src/pages/MovieDetails.js
@@ -1,7 +1,58 @@
+import React, { useState, useEffect } from "react";
+import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from "react-bootstrap";
+
 function MovieDetails() {
+  // create state for holding returned doesthedogdie api data
+  const [searchedDealbreakers, setSearchedDealbreakers] = useState([]);
+
+  useEffect(() => {
+    getDealbreakers();
+  }, []);
+
+  const getDealbreakers = async () => {
+    try {
+      const options = {
+        method: "GET",
+        crossDomain: true,
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          "X-API-KEY": process.env.REACT_APP_DDD_KEY,
+        },
+      };
+
+      // TODO: remove this hard coded variable when I get the movie search link set up
+      const dddId = 10752;
+
+      // TODO: remove this when not working on localhost
+      const corsAnywhere = `https://cors-anywhere.herokuapp.com/`;
+
+      const url = `${corsAnywhere}https://www.doesthedogdie.com/media/${dddId}`;
+
+      console.log(url);
+
+      const response = await fetch(url, options);
+
+      console.log(response);
+
+      const { items } = await response.json();
+
+      console.log(items);
+
+      const dealbreakerList = items;
+
+      console.log(dealbreakerList);
+
+      // setSearchedDealbreakers(dealbreakerList);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   return (
     <>
       <h1>This is the Movie Details page</h1>
+      {searchedDealbreakers}
     </>
   );
 }
