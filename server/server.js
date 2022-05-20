646d144d6622b556957ecfc4c55a9a74a947fac8
NotANewt <meegafauna@gmail.com>
2022-05-19 20:27:39 -0500

    took out build

--- server/server.js
+++ server/server.js
@@ -19,11 +19,11 @@ app.use(express.json());
 
 // Serve up static assets
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
+  app.use(express.static(path.join(__dirname, "../client")));
 }
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build"));
+  res.sendFile(path.join(__dirname, "../client"));
 });
 
 // Create a new instance of an Apollo server with the GraphQL schema
