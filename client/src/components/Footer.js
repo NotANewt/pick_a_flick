f66c3f4e328768c7aaaca709a8175a4287e8b193
NotANewt <meegafauna@gmail.com>
2022-05-21 15:09:48 -0500

    udpated footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,10 +1,25 @@
-import { Card, Container, Button, ButtonGroup, Row, Col, Stack, Form, Badge } from "react-bootstrap";
+import { Card, Container, Button, ButtonGroup, Row, Col, Stack, Form, Badge, ListGroup } from "react-bootstrap";
 
 function Footer() {
   return (
     <>
-      <hr />
-      <p>Made with 💙 and the MERN stack by Meegan Anderson 2022 </p>
+      <footer className="text-center text-lg-start bg-dark">
+        <ul className="d-flex justify-content-center py-2">
+          <li>
+            <a href="https://github.com/NotANewt" target="_blank" className="btn btn-primary btn-md btn-floating mx-2 btn-social">
+              <i className="fab fa-github"></i>
+            </a>
+          </li>
+
+          <li>
+            <a href="https://www.linkedin.com/in/meegan-anderson-753634112/" target="_blank" className="btn btn-primary btn-md btn-floating mx-2 btn-social">
+              <i className="fab fa-linkedin"></i>
+            </a>
+          </li>
+        </ul>
+
+        <div className="text-center text-white pb-2 text-copyright">&copy; 2022 Made with 💙 and the MERN stack by Meegan </div>
+      </footer>
     </>
   );
 }
