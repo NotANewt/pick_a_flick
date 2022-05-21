38767f60056a7b426523bfcef7065da92fae4f88
NotANewt <meegafauna@gmail.com>
2022-05-21 13:03:16 -0500

    updated home page to have button links to profile, dealbreakers, and movie search when user is logged in

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,19 +1,39 @@
 import { Link } from "react-router-dom";
 import { Container, Col, Row, Form, Button, Card } from "react-bootstrap";
 
+import Auth from "../utils/auth";
+
 function Home(props) {
   return (
     <>
       <Container>
         <h1>Welcome to Pick A Flick!</h1>
-        <Card>
-          <Card.Body>
-            <Card.Text>Log in or sign up to get started.</Card.Text>
-            <Link as={Link} to="/LoginSignup">
-              Login or Signup
-            </Link>
-          </Card.Body>
-        </Card>
+        {Auth.loggedIn() ? (
+          <Card>
+            <Card.Body>
+              <div className="d-grid gap-2">
+                <Button variant="outline-primary" href="/Profile">
+                  Check out your Profile
+                </Button>
+                <Button variant="outline-primary" href="/Dealbreakers">
+                  Search for Dealbreakers to add them to your Profile
+                </Button>
+                <Button variant="outline-primary" href="/Movies">
+                  Search for Movies add them to your Profile
+                </Button>
+              </div>
+            </Card.Body>
+          </Card>
+        ) : (
+          <Card>
+            <Card.Body>
+              <Card.Text>Log in or sign up to get started.</Card.Text>
+              <Link as={Link} to="/LoginSignup">
+                Login or Signup
+              </Link>
+            </Card.Body>
+          </Card>
+        )}
       </Container>
     </>
   );
